38d417b797226b0e1711d6f731b23dbe7135df7a
leandro <leanvallejos666@gmail.com>
2025-04-05 19:16:47 -0300

    conexion a la bd

---
+++ frontend/personal-finance/src/services/gasto.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Gasto } from '../app/models/gasto';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GastoService {
+  private apiUrl = 'http://localhost:8000/api/expenses/'; // Ajusta la URL según tu backend
+
+  constructor(private http: HttpClient) {}
+
+  // Obtener todos los gastos
+  getGastos(): Observable<Gasto[]> {
+    return this.http.get<Gasto[]>(this.apiUrl);
+  }
+
+  // Agregar un nuevo gasto
+  agregarGasto(gasto: Gasto): Observable<Gasto> {
+    return this.http.post<Gasto>(this.apiUrl, gasto);
+  }
+
+  // Editar un gasto existente
+  editarGasto(id: number, gasto: Gasto): Observable<Gasto> {
+    return this.http.put<Gasto>(`${this.apiUrl}${id}/`, gasto);
+  }
+
+  // Eliminar un gasto
+  eliminarGasto(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}${id}/`);
+  }
+}
